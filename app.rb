e0d75c687ddd61d8496804b5f2d3a80916230487
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:48:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "Hello, World!"
+  end
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/' do
     "Hello, World!"
   end
   
