f2790e7c202d1cd17706ab1062835ba155104124
Sean Altemose <sean@seanaltemose.com>
2018-06-26 19:50:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Sean."
+  end
+
+  get '/hometown' do
+    "My hometown is Nazareth, PA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Throb, by Janet Jackson."
+  end
+
 end
